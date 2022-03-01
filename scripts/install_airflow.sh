7216a74c4a0edde5e7071a00180794e2ae1ffc18
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2022-03-01 03:19:50 -0800

    Update install_airflow.sh

--- scripts/install_airflow.sh
+++ scripts/install_airflow.sh
@@ -4,7 +4,7 @@ set -e
 export AIRFLOW_HOME=~/airflow
 # Install Airflow using the constraints file
 AIRFLOW_VERSION=2.2.4
-PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.6
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.2.2/constraints-3.6.txt
