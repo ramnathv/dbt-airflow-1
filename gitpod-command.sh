e4481b76ebc3e07b3e082f47c2a0027b5c7e17c6
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2022-03-01 03:07:37 -0800

    Create gitpod-command.sh

---
+++ gitpod-command.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Running the command script"
+mkdir -p /workspace/.dbt
+ln -snf /workspace/.dbt ~/.dbt
+
+export PGPASSWORD=gitpod PGDATABASE=dbt
+export AIRFLOW__CORE__DAGS_FOLDER=/workspace/test-dbt-airflow/airflow/dags
+export AIRFLOW__CORE__LOAD_EXAMPLES=False
