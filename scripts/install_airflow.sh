91ec5cc3b90f9a87f6e781821134937cd758d17a
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2022-03-01 02:57:27 -0800

    Create install_airflow.sh

---
+++ scripts/install_airflow.sh
@@ -0,0 +1,12 @@
+export AIRFLOW_HOME=~/airflow
+
+# Install Airflow using the constraints file
+AIRFLOW_VERSION=2.2.4
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+# For example: 3.6
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.2.2/constraints-3.6.txt
+# echo "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install --upgrade pip
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}" --user
+pip install dag-factory airflow-dbt pandas
